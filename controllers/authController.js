8aec9548283f35c5c02a21d17eba3f063a6cf26b
yashwalhekar <yashwalhekar371@gmail.com>
2025-11-27 13:48:58 +0530

    fix bugs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,4 @@
-
-import User from "../models/User.js";
+import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 
 const generateToken = (id, role) => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 const generateToken = (id, role) => {
   return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "7d" });
